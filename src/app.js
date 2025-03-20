1f853aa1c266c10fba5a5d6524d66e67ab447ad4
shrirang3 <shrirang.mahankaliwar21@vit.edu>
2025-03-20 18:04:46 +0530

    routes setup

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.urlencoded({extended:true, limit: '16kb'}))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+//import routes
+import userRoute from './routes/user.routes.js'
+//routes declaration
+app.use("/api/v1/users", userRoute)   // http://localhost:8000/api/v1/users/register
+
+
+
+
 export { app }
